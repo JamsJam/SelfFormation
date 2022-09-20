c0b38656eff5182d645329e87b16c56adeb9deaf
Jams <Jamsjam971@hotmail.com>
2022-09-20 00:46:06 -0400

    first commit

---
+++ Javascript/1-lesBases/1-variable.js
@@ -0,0 +1,49 @@
+// IL existe 2 moyens declarer une variable : let & var
+
+// ***********
+// var -> variable global
+// let -> variable local
+
+// On essaye d'utiliszer au maximum le mot-cle let
+// ***********
+
+var prenom ;
+let nom;
+
+// ***************** On va maintenant assigner une valeur au variables
+// le "=" permet d'assigner une valeur a la variable.
+prenom = "Marc";
+nom = "Pierre";
+
+console.log(prenom); // sortie : Marc
+console.log(nom); // sortie : Pierre
+
+
+
+
+
+
+
+// ************** Les types de données
+
+
+
+let couleur = "jaune";
+// Chaine de caractere ou String (avec les ""  ou les '' )
+
+let age = 18 ;
+// Nombre ( des chiffres sans "" ou '' deviendront automatiquement de type nombre)
+
+let win = true;
+    win = false
+// Booleen ou Boolean, a pour valeur vrai ( true) ou faux(false)
+
+
+
+
+
+
+//*************** Les constantes */
+
+const titre = "le Titre"
+// Une  constante se declare avec "const", une constante ne peux pas changer de valeur
---
+++ Javascript/1-lesBases/2-concatenation.js
@@ -0,0 +1,20 @@
+const prenom = "Jeremy"
+
+console.log(prenom) //sortie : jeremy
+
+
+
+//Pour concatener on vza utiliser le "+" de la maniere suivante
+
+console.log("bonjour"+prenom+"comment ça va ?") // Sortie :  bonjourjeremycomment ça va ?"
+console.log("bonjour "+prenom+" comment ça va ?") // Sortie :  bonjour jeremy comment ça va ?"
+
+
+
+// Exercice afficher bonjour [prenom] [nom]
+
+const nom = "Antoine"
+
+console.log("bonjour "+prenom+" "+nom+" comment ça va ?") // Sortie :  bonjour jeremy Antoine comment ça va ?"
+
+
